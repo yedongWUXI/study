5323ad0a0b30b73970f5dedadcfb536cdcb303c8
yedongWUXI <zhyd704@126.com>
2018-07-25 18:17:46 +0800

    radix

---
+++ src/test/java/com/radix_demo/CommonTest.java
@@ -0,0 +1,21 @@
+package com.radix_demo;
+
+import org.junit.Test;
+
+/**
+ * @Description: java类作用描述
+ * @Author: wxzhangyd
+ * @Date: 2018/7/9 13:40
+ * @Modified by:
+ */
+
+public class CommonTest {
+    @Test
+    public void test(){
+        /**
+         * radix 多少进制
+         */
+        int i = Integer.parseInt("1010", 2);
+        System.out.println(i);
+    }
+}
