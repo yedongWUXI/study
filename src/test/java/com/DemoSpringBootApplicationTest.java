5845d53f045a8af3eac8539312b511e329a4bd45
zhangyedong <wxzhangyd@yutong.com>
2018-09-29 14:12:54 +0800

    springboot mail

---
+++ src/test/java/com/DemoSpringBootApplicationTest.java
@@ -0,0 +1,16 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+/**
+ * @author yeodng
+ */
+public class DemoSpringBootApplicationTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringBootApplicationTest.class, args);
+    }
+
+}
---
+++ src/test/java/com/SpringBootTest.java
@@ -0,0 +1,46 @@
+package com;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@org.springframework.boot.test.context.SpringBootTest
+/**
+ * @author yeodng
+ */
+public class SpringBootTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTest.class, args);
+    }
+
+
+    @Autowired
+    private JavaMailSender mailSender; //自动注入的Bean
+
+    @Value("${spring.mail.username}")
+    private String Sender; //读取配置文件中的参数
+
+    /**
+     * 简单测试发送邮件
+     *
+     * @throws Exception
+     */
+    @Test
+    public void sendSimpleMail() throws Exception {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(Sender);
+        message.setTo("1280125483@qq.com");
+        message.setSubject("主题：简单邮件");
+        message.setText("国庆去哪玩呀");
+        mailSender.send(message);
+    }
+
+
+}
