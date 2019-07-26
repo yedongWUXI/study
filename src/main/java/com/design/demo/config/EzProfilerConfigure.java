ce3e53e046ef05382335be9193bdcd8b0285a71a
yedong <zhyd704@126.com>
2019-07-26 14:04:08 +0800

    logback 加入 EzProfilerConfigure测试

---
+++ src/main/java/com/design/demo/config/EzProfilerConfigure.java
@@ -0,0 +1,15 @@
+//package com.design.demo.config;
+//
+//import com.github.xjs.ezprofiler.annotation.EnableProfiler;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * @Description: java类作用描述
+// * @Author: yedong
+// * @Date: 2019/7/2 16:32
+// * @Modified by:
+// */
+//@EnableProfiler
+//@Configuration
+//public class EzProfilerConfigure {
+//}
