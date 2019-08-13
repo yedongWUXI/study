5a1321c2fca34a1636da1e74acb34c317f3cd8fb
yedong <zhyd704@126.com>
2019-08-13 15:17:38 +0800

    stream add peek test

--- src/test/java/com/java8/stream/StreamTest.java
+++ src/test/java/com/java8/stream/StreamTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -21,14 +22,43 @@ public class StreamTest {
                 new Dish("rice", true, 800, Dish.Type.OTHER),
                 new Dish("season", true, 800, Dish.Type.OTHER),
                 new Dish("pizza", true, 800, Dish.Type.OTHER),
-                new Dish("prawns", false, 800, Dish.Type.FISH),
+                new Dish("prawns", false, 1800, Dish.Type.FISH),
+                new Dish("prawns", false, 1800, Dish.Type.FISH),
                 new Dish("salmon", false, 800, Dish.Type.FISH)
         );
 
-        String string = Arrays.toString(menu.stream().filter(Dish::getVegetarian).collect(Collectors.toList()).toArray());
+
+        List<Person> peopleList = Arrays.asList(
+                new Person("pork", 1, "zs")
+//                ,
+//                new Person("beef", 2, "ls"),
+//                new Person("chicken", 3, "ww"),
+//                new Person("french", 4, "zl"),
+//                new Person("rice", 5, "qg"),
+//                new Person("season", 6, "wyx")
+        );
+
+
+        String string = Arrays.toString(menu.stream().filter(p -> p.getCalories() > 1000).collect(Collectors.toList()).toArray());
         System.out.println(string);
 
-    }
+        Map<String, Dish> dishMap = menu.stream().collect(Collectors.toMap(Dish::getName, v -> v, (v1, v2) -> v1));
 
+
+        List<Person> collect = peopleList.stream().filter(p -> dishMap.containsKey(p.getDishName())).collect(Collectors.toList());
+
+        System.out.println(Arrays.toString(collect.toArray()));
+        //peek里 用{}设置多个属性
+        List<Person> collect1 = peopleList.stream().peek(v -> {
+            v.setDishCalories(dishMap.get(v.getDishName()).getCalories());
+            v.setName("");
+        }).collect(Collectors.toList());
+
+        System.out.println(collect1);
+
+//        System.out.println(dishMap);
+
+
+    }
 
 }
